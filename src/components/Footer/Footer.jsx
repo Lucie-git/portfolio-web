de251aae5bd5032ec676ae3b947f6cd778d5b3e4
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-06-27 11:51:53 +0200

    Various changes Install react router Add background img Little corrections

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <span className="footer-title">© 2025 Lucie Vadkerti</span>
-      <a class="footer-link" href="tel:+420774868955">
+      <a className="footer-link" href="tel:+420774868955">
         +420 774 868 955
       </a>
       <a className="footer-link" href="mailto:lucie.vadkerti@gmail.com">
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -5,19 +5,6 @@ export const App = () => {
   return (
     <>
       <Header />
-      <div>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto natus nobis modi
-        blanditiis consequatur sit, consequuntur eius quaerat praesentium officiis a unde temporibus
-        et incidunt amet commodi. Officiis, eaque voluptatem! Lorem ipsum dolor sit, amet
-        consectetur adipisicing elit. Architecto natus nobis modi blanditiis consequatur sit,
-        consequuntur eius quaerat praesentium officiis a unde temporibus et incidunt amet commodi.
-        Officiis, eaque voluptatem! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        Architecto natus nobis modi blanditiis consequatur sit, consequuntur eius quaerat
-        praesentium officiis a unde temporibus et incidunt amet commodi. Officiis, eaque voluptatem!
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Architecto natus nobis modi
-        blanditiis consequatur sit, consequuntur eius quaerat praesentium officiis a unde temporibus
-        et incidunt amet commodi. Officiis, eaque voluptatem!
-      </div>
       <Footer />
     </>
   );
