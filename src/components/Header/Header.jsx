5b1218cad9e76e75ed2795d2d3de015546ce84d2
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-07-02 18:46:30 +0200

    OnClick doesnt work, try to solve it

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,38 +6,34 @@ export const Header = () => {
   const [menuOpened, setMenuOpened] = useState(false);
   console.log(menuOpened);
 
+  const handleClick = () => {
+    console.log('Tlačítko funguje');
+    setMenuOpened(!menuOpened);
+  };
+
   return (
     <>
       <header className="header">
         <img src="assets/logo_my_big.png" alt="Osobní logo" />
-        <div
-          tabIndex={0}
-          className="hamburger"
-          onClick={() => {
-            setMenuOpened(!menuOpened);
-            console.log('funguju');
-          }}
-        >
+        <button onClick={handleClick}>klik</button>
+        <div className="hamburger">
           <div className="bar"></div>
           <div className="bar"></div>
           <div className="bar"></div>
         </div>
-
-        {menuOpened && (
-          <nav className="header-nav">
-            <ul>
-              <Link to={'/about'} className="nav-link">
-                About
-              </Link>
-              <Link to={'/projects'} className="nav-link">
-                Projects
-              </Link>
-              <Link to={'/contacts'} className="nav-link">
-                Contacts
-              </Link>
-            </ul>
-          </nav>
-        )}
+        <nav className="header-nav">
+          <ul>
+            <Link to={'/about'} className="nav-link">
+              About
+            </Link>
+            <Link to={'/projects'} className="nav-link">
+              Projects
+            </Link>
+            <Link to={'/contacts'} className="nav-link">
+              Contacts
+            </Link>
+          </ul>
+        </nav>
       </header>
     </>
   );
