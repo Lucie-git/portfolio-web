afa318980266d0107f159f74a4130ee8df259eb5
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-07-02 21:59:17 +0200

    Add Main component & relevant edits

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import './Main.css';
+
+export const Main = () => {
+  return (
+    <>
+      <h1>Lucie Vadkerti</h1>
+      <h2>PORTFOLIO</h2>
+    </>
+  );
+};
--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,14 +1,27 @@
-import { Outlet } from 'react-router-dom';
+import './index.css';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Footer } from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
+import { Main } from '../components/Main/Main';
 
 export const App = () => {
+  const location = useLocation();
+  const isMain = location.pathname === '/';
+
   return (
     <>
       <Header />
-      <div className="container">
-        <Outlet />
-      </div>
+      {isMain ? (
+        <div className="main-container">
+          {' '}
+          <Main />
+        </div>
+      ) : (
+        <div className="container">
+          <Outlet />
+        </div>
+      )}
+
       <Footer />
     </>
   );
