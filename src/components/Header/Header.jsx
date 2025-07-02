cacfb5682d0992e2d010c96e82bd32cf5a713e1f
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-07-02 16:57:35 +0200

    Update react, work on Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,43 @@
+import { useState } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 
 export const Header = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  console.log(menuOpened);
+
   return (
     <>
       <header className="header">
         <img src="assets/logo_my_big.png" alt="Osobní logo" />
-        <nav className="header-nav">
-          <Link to={'/about'} className="nav-link">
-            About
-          </Link>
-          <Link to={'/projects'} className="nav-link">
-            Projects
-          </Link>
-          <Link to={'/contacts'} className="nav-link">
-            Contacts
-          </Link>
-        </nav>
+        <div
+          tabIndex={0}
+          className="hamburger"
+          onClick={() => {
+            setMenuOpened(!menuOpened);
+            console.log('funguju');
+          }}
+        >
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+
+        {menuOpened && (
+          <nav className="header-nav">
+            <ul>
+              <Link to={'/about'} className="nav-link">
+                About
+              </Link>
+              <Link to={'/projects'} className="nav-link">
+                Projects
+              </Link>
+              <Link to={'/contacts'} className="nav-link">
+                Contacts
+              </Link>
+            </ul>
+          </nav>
+        )}
       </header>
     </>
   );
