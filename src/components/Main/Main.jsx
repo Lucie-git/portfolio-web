ba7c8f52a5d920556dbd7392417cd012acfcf697
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-07-03 17:00:15 +0200

    Partial style changes

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,8 +3,8 @@ import './Main.css';
 export const Main = () => {
   return (
     <>
-      <h1>Lucie Vadkerti</h1>
-      <h2>PORTFOLIO</h2>
+      <h1 className="main-header">Lucie Vadkerti</h1>
+      <h2 className="main-header">PORTFOLIO</h2>
     </>
   );
 };
