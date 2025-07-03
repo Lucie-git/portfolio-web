d0d0488ef339ad82ed968f679d6895a0b633db51
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-07-03 11:27:14 +0200

    Various changes 2nd Add qr code bussiness card Edit Contacts component structure Edit text color

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,18 +4,24 @@ export const Contacts = () => {
   return (
     <>
       <h1 className="contacts-header">Contacts</h1>
-      <p className="contacts">
-        E-mail: <a href="mailto:lucie.vadkerti@gmail.com">lucie.vadkerti@gmail.com</a>
-      </p>
-      <p className="contacts">
-        Phone: <a href="tel:+420774868955">+420 774 868 955</a>
-      </p>
-      <p className="contacts">
-        Linked In: <a href="https://www.linkedin.com/in/lucie-vadkerti/">Lucie Vadkerti</a>
-      </p>
-      <p className="contacts">
-        Git Hub: <a href="https://github.com/Lucie-git">Lucie-git</a>
-      </p>
+      <p className="contacts">E-mail: </p>
+      <a className="contact-links" href="mailto:lucie.vadkerti@gmail.com">
+        lucie.vadkerti@gmail.com
+      </a>
+      <p className="contacts">Phone: </p>
+      <a className="contact-links" href="tel:+420774868955">
+        +420 774 868 955
+      </a>
+      <p className="contacts">Linked In: </p>
+      <a className="contact-links" href="https://www.linkedin.com/in/lucie-vadkerti/">
+        /in/lucie-vadkerti/
+      </a>
+      <p className="contacts"> Git Hub: </p>
+      <a className="contact-links" href="https://github.com/Lucie-git">
+        Lucie-git
+      </a>
+      <p className="contacts">Bussines card:</p>
+      <img src="/assets/qr-code.png" alt="QR vizitka" />
     </>
   );
 };
