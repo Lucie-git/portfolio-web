dd484a55e0fb045cd805096316d3625e5697704c
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-07-03 16:59:19 +0200

    Add content & styles for Project component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,53 @@
 import './Projects.css';
 
 export const Projects = () => {
-  return <h1>Projects</h1>;
+  return (
+    <>
+      <h1 className="project-header">Projects</h1>
+      <h2 className="project-header">
+        <a target="_blank" href="https://my-shelfie.netlify.app/">
+          Shelfie
+        </a>
+      </h2>
+      <h3 className="project-header">Personal library</h3>
+      <p className="project-description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam natus, voluptatibus rerum
+        praesentium delectus sapiente. Voluptatum ducimus nihil, modi error excepturi veritatis
+        itaque facere natus fuga debitis provident animi deleniti!
+      </p>
+      <h2 className="project-header">
+        <a target="_blank" href="https://lucie-git.github.io/o-projektu/ ">
+          About Shelfie
+        </a>
+      </h2>
+      <h3 className="project-header">Web presentaion</h3>
+      <p className="project-description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam natus, voluptatibus rerum
+        praesentium delectus sapiente. Voluptatum ducimus nihil, modi error excepturi veritatis
+        itaque facere natus fuga debitis provident animi deleniti!
+      </p>
+      <h2 className="project-header">
+        <a target="_blank" href="https://lucie-git.github.io/lucie-na-cestach/ ">
+          On the way
+        </a>
+      </h2>
+      <h3 className="project-header">Introduction to HTML and CSS</h3>
+      <p className="project-description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam natus, voluptatibus rerum
+        praesentium delectus sapiente. Voluptatum ducimus nihil, modi error excepturi veritatis
+        itaque facere natus fuga debitis provident animi deleniti!
+      </p>
+      <h2 className="project-header">
+        <a target="_blank" href="https://github.com/?q=projekt ">
+          Other
+        </a>
+      </h2>
+      <h3 className="project-header">Additional projects during studies</h3>
+      <p className="project-description">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam natus, voluptatibus rerum
+        praesentium delectus sapiente. Voluptatum ducimus nihil, modi error excepturi veritatis
+        itaque facere natus fuga debitis provident animi deleniti!
+      </p>
+    </>
+  );
 };
