5956ae294b02c005c3e1b2a336a7dcfcbb3a6915
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-07-03 12:51:10 +0200

    Improve responsive styles & add grid

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -4,24 +4,26 @@ export const Contacts = () => {
   return (
     <>
       <h1 className="contacts-header">Contacts</h1>
-      <p className="contacts">E-mail: </p>
-      <a className="contact-links" href="mailto:lucie.vadkerti@gmail.com">
-        lucie.vadkerti@gmail.com
-      </a>
-      <p className="contacts">Phone: </p>
-      <a className="contact-links" href="tel:+420774868955">
-        +420 774 868 955
-      </a>
-      <p className="contacts">Linked In: </p>
-      <a className="contact-links" href="https://www.linkedin.com/in/lucie-vadkerti/">
-        /in/lucie-vadkerti/
-      </a>
-      <p className="contacts"> Git Hub: </p>
-      <a className="contact-links" href="https://github.com/Lucie-git">
-        Lucie-git
-      </a>
-      <p className="contacts">Bussines card:</p>
-      <img className="qr-code" src="/assets/qr-code.png" alt="QR vizitka" />
+      <div className="contacts-container">
+        <p className="contacts">E-mail: </p>
+        <a className="contact-links" href="mailto:lucie.vadkerti@gmail.com">
+          lucie.vadkerti@gmail.com
+        </a>
+        <p className="contacts">Phone: </p>
+        <a className="contact-links" href="tel:+420774868955">
+          +420 774 868 955
+        </a>
+        <p className="contacts">Linked In: </p>
+        <a className="contact-links" href="https://www.linkedin.com/in/lucie-vadkerti/">
+          /in/lucie-vadkerti/
+        </a>
+        <p className="contacts"> Git Hub: </p>
+        <a className="contact-links" href="https://github.com/Lucie-git">
+          Lucie-git
+        </a>
+        <p className="contacts">Bussines card:</p>
+        <img className="qr-code" src="/assets/qr-code.png" alt="QR vizitka" />
+      </div>
     </>
   );
 };
